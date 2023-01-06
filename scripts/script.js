f7fd22834866c7dfbb0c28732d4369a5a8b373ce
tdera06 <62854732+tymekalil@users.noreply.github.com>
2023-01-06 14:43:57 +0100

    -

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,17 @@
 
 import data from "./cards-troops.js";
 
+const answersEl = document.querySelector(".answer");
+
 const cards = data.cards;
 const card = cards[0];
+
+const cardKeys = Object.keys(card);
+const cardSize = cardKeys.length;
+
+for (let i = 1; i < cardSize - 5; i++) {
+  const tile = document.createElement("div");
+  tile.className = "card-tile";
+  tile.innerHTML = card[cardKeys[i]];
+  answersEl.appendChild(tile);
+}
